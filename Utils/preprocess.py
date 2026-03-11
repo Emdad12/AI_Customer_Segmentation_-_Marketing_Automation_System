51c6322189d3fc0523a993d04bfb0275866f1dd5
Md Emdadul haque <emdademu28@gmail.com>
2026-03-11 23:27:45 +0600

    Delete Utils directory

--- Utils/preprocess.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def transform_features(df):
-
-    df["monetary"] = np.log1p(df["monetary"])
-
-    df["avg_order_value"] = np.log1p(df["avg_order_value"])
-
-    return df
--- Utils/segmentations.py
+++
@@ -1,6 +0,0 @@
-segment_map = {
-0:"Churned / Lost Customers",
-1:"VIP / Champions",
-2:"Inactive Discount Seekers",
-3:"Active Regulars"
-}
